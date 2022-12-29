10e4ab6242521f28a587b1d74f3ac5d65948a195
Isaac Farias <isaacfarias006@gmail.com>
2022-12-29 13:09:16 -0300

    Adiciona paciente dto

---
+++ ResidenciaDesafio1/PacienteDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResidenciaDesafio1
+{
+    public class PacienteDTO
+    {
+        public string Nome { get; set; } = string.Empty;
+        public string CPF { get; set; } = string.Empty;
+        public DateTime DataNascimento { get; set; } = DateTime.MinValue;
+    }
+}
