cda600cb4ba4c3173402167f1614ea9ec7c7695d
Isaac Farias <isaacfarias006@gmail.com>
2022-12-29 13:09:03 -0300

    Adiciona classe para guardar erros de validação de paciente

---
+++ ResidenciaDesafio1/PacienteErrors.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResidenciaDesafio1
+{
+    public class PacienteErrors
+    {
+        private readonly Dictionary<PacienteField, string> errors;
+
+        public PacienteErrors()
+        {
+            errors = new();
+        }
+
+        public void AddError(PacienteField field, string message)
+        {
+            errors.Add(field, message);
+        }
+
+        public void Clear()
+        {
+            errors.Clear();
+        }
+        public bool IsEmpty => errors.Count == 0;
+
+        public bool HasError(PacienteField field)
+        {
+            return errors.TryGetValue(field, out var _);
+        }
+
+        public string GetErrorMessage(PacienteField field)
+        {
+            return HasError(field) ? errors[field] : string.Empty;
+        }
+    }
+}
