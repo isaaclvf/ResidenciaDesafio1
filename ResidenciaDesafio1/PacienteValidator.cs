f17f001de7cdef89fc0b8528439f9e63fb23e359
Isaac Farias <isaacfarias006@gmail.com>
2023-01-02 09:27:11 -0300

    Adiciona classe PacienteValidator

---
+++ ResidenciaDesafio1/PacienteValidator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResidenciaDesafio1
+{
+    public class PacienteValidator
+    {
+        private readonly PacienteErrors errors = new PacienteErrors();
+        public PacienteDTO Paciente { get; private set; }
+
+        public PacienteValidator()
+        {
+            Paciente = new();
+        }
+
+        public PacienteErrors Errors { get { return errors; } }
+
+        public bool IsValid(string nome, string cpf, string strDataNascimento)
+        {
+            errors.Clear();
+
+            // Nome 
+            nome = nome.Trim();
+            if (nome.Length < 5)
+                errors.AddError(PacienteField.NOME, "Nome deve ter ao menos 5 letras");
+            else
+                Paciente.Nome = nome;
+
+            // CPF
+            cpf = cpf.Trim();
+            if (!cpf.IsValidCPF())
+                errors.AddError(PacienteField.CPF, "CPF inválido");
+            // TODO: Fazer uma condição se o paciente já estiver no cadastro
+            else
+                Paciente.CPF = cpf;
+
+            // Data de Nascimento
+            try
+            {
+                Paciente.DataNascimento = DateTime.ParseExact(
+                    strDataNascimento, 
+                    "dd/MM/yyyy", 
+                    System.Globalization.CultureInfo.InvariantCulture);
+
+                if (Paciente.DataNascimento > DateTime.Now.AddYears(-13))
+                    errors.AddError(PacienteField.DATA_NASCIMENTO, 
+                        "O paciente deve ter pelo menos 13 anos no momento do cadastro");
+            }
+            catch (Exception)
+            {
+                errors.AddError(PacienteField.DATA_NASCIMENTO, 
+                    "Data de nascimento deve estar no formato DD/MM/AAAA");
+            }
+
+            return errors.IsEmpty;
+        }
+    }
+}
