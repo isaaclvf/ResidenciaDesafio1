35fdb68132b3ecb0beafcfaf0dfec4f1707d797d
Isaac Farias <isaacfarias006@gmail.com>
2023-01-02 09:26:24 -0300

    Adiciona classe Paciente

---
+++ ResidenciaDesafio1/Paciente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResidenciaDesafio1
+{
+    public class Paciente
+    {
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+        public DateTime DataNascimento { get; private set; }
+
+        public Paciente(string nome, string cPF, DateTime dataNascimento)
+        {
+            Nome = nome;
+            CPF = cPF;
+            DataNascimento = dataNascimento;
+        }
+    }
+}
