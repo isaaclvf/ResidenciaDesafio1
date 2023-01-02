fe6df5834ade9e6f124dcfa0039e078666ffe88f
Isaac Farias <isaacfarias006@gmail.com>
2023-01-02 13:49:24 -0300

    Conserta lógica de validação

--- ResidenciaDesafio1/Extensions.cs
+++ ResidenciaDesafio1/Extensions.cs
@@ -10,39 +10,42 @@ namespace ResidenciaDesafio1
     {
         public static bool IsValidCPF(this string cpf)
         {
-            // 11 dígitos
+            string digito;
+            int soma;
+            int resto;
+
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+
             if (cpf.Length != 11)
                 return false;
 
-            // Todos iguais
-            if (cpf.All(digito => digito == cpf[0]))
-                return false;
-
-            // Checar primeiro dígito verificador (j)
-            int j = 0;
-            int somaJ = 0;
+            soma = 0;
             for (int i = 0; i < 9; i++)
-                somaJ += (int)Char.GetNumericValue(cpf[i]) * (10 - i);
+                soma += int.Parse(cpf[i].ToString()) * (10 - i);
 
-            if (!(somaJ % 11 == 0 || somaJ % 11 == 1))
-                j = 11 - (somaJ % 11);
+            resto = soma % 11;
 
-            if ((int)Char.GetNumericValue(cpf[9]) != j)
-                return false;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
 
-            // Checar segundo dígito verificador (k)
-            int k = 0;
-            int somaK = 0;
-            for (int i = 0; i < 9; i++)
-                somaK += (int)Char.GetNumericValue(cpf[i]) * (11 - i);
+            digito = resto.ToString();
 
-            if (!(somaK % 11 == 0 || somaK % 11 == 1))
-                k = 11 - (somaK % 11);
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(cpf[i].ToString()) * (11 - i);
 
-            if ((int)Char.GetNumericValue(cpf[10]) != k)
-                return false;
+            resto = soma % 11;
 
-            return true;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+
+            digito = digito + resto.ToString();
+            return cpf.EndsWith(digito);
         }
     }
 }
