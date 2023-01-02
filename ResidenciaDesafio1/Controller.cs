41fc389b713fc9d92a51074ad34ff37d759f15a5
Isaac Farias <isaacfarias006@gmail.com>
2023-01-02 09:32:43 -0300

    Adiciona opções de cadastro de paciente no controller

--- ResidenciaDesafio1/Controller.cs
+++ ResidenciaDesafio1/Controller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,82 @@ namespace ResidenciaDesafio1
         public static void Start()
         {
             var menuForm = new MenuForm();
-            var escolha = menuForm.ReadMenuPrincipal();
-            Console.WriteLine(escolha);
+            // TODO: Instanciar um objeto Cadastro
+
+            while (menuForm.OpcPrincipal != 3)
+            {
+                Console.Clear();
+                menuForm.ReadMenuPrincipal();
+
+                // Cadastro de pacientes
+                if (menuForm.OpcPrincipal == 1)
+                {
+                    Console.Clear();
+                    menuForm.ReadMenuCadastro();
+
+                    // Cadastrar novo paciente
+                    if (menuForm.OpcCadastro == 1)
+                    {
+                        Console.Clear();
+                        var paciente = ReadPaciente();
+                        // Adicionar paciente ao cadastro
+                        Console.ReadLine();
+                    }
+
+                    // Excluir paciente
+
+                    // Listar pacientes (CPF)
+
+                    // Listar pacientes (Nome)
+
+                    // Voltar
+                    if (menuForm.OpcCadastro == 5)
+                        continue;
+                }
+
+                // Agenda
+                if (menuForm.OpcPrincipal == 2)
+                {
+                    Console.Clear();
+                    menuForm.ReadMenuAgenda();
+
+                    // Agendar consulta
+
+                    // Cancelar agendamento
+
+                    // Listar agenda
+
+                    // Voltar
+                    if (menuForm.OpcAgenda == 4)
+                        continue;
+                }
+            }
+        }
+
+        public static Paciente ReadPaciente()
+        {
+            bool isValid = false;
+            var form = new PacienteForm();
+            var validador = new PacienteValidator();
+            Paciente? paciente = null;
+
+            form.ReadData();
+
+            while (!isValid)
+            {
+                isValid = validador.IsValid(form.Nome, form.CPF, form.DataNascimento);
+
+                if (isValid)
+                {
+                    paciente = new Paciente(validador.Paciente.Nome, validador.Paciente.CPF, validador.Paciente.DataNascimento);
+                }
+                else
+                {
+                    form.ReadData(validador);
+                }
+            }
+
+            return paciente;
         }
     }
 }
