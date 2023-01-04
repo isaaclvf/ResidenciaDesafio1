337371953341b30dfc39f9cd3eb97d5dc97f03c3
Isaac Farias <isaacfarias006@gmail.com>
2023-01-03 22:46:07 -0300

    Adiciona classe de agendamento

---
+++ ResidenciaDesafio1/Agendamento.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResidenciaDesafio1
+{
+    public class Agendamento
+    {
+        public string CPF { get; private set; }
+        public DateOnly Data { get; private set; }
+        public TimeOnly HoraInicial { get; private set; }
+        public TimeOnly HoraFinal { get; private set; }
+
+        public Agendamento(string cpf, DateOnly data, TimeOnly horaInicial, TimeOnly horaFinal)
+        {
+            CPF = cpf;
+            Data = data;
+            HoraInicial = horaInicial;
+            HoraFinal = horaFinal;
+        }
+    }
+}
